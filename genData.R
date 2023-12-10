828dd1f22291a23b9b85ef5461832dd05bef88d3
Vadim Pletzer <vadim.pletzer@gmail.com>
2023-12-10 16:02:27 +1300

    adding function to generate survival data

---
+++ genData.R
@@ -0,0 +1,8 @@
+genData <- function(npeople, lambda, tau){
+	#param: npeople, number of people
+	#param: lambda, the hazard
+	#param: tau, the time people are followed (same for everyone)
+	#return: dataframe with cols, time_to_event and censored (1 = yes, 0 = no)
+
+	
+}
